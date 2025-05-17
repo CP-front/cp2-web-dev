50da152cd964d36e4780ad927abec2e0c132feb2
Rodrigo <rodrigocardosotadeo@gmail.com>
2025-05-17 18:48:01 -0300

    adicionando o slide show no projeto

---
+++ src/js/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const slides = document.querySelectorAll('.item');
+    const dots = document.querySelectorAll('.dot');
+    let currentSlide = 0;
+    let slideInterval;
+
+    function initSlideshow() {
+        if (slides.length === 0) return;
+              
+        startSlideshow();
+        
+        dots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                currentSlide = index;
+                updateSlideshow();
+                resetInterval();
+            });
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        updateSlideshow();
+    }
+
+    function updateSlideshow() {
+        slides.forEach(slide => slide.classList.remove('active'));
+        dots.forEach(dot => dot.classList.remove('active'));
+        
+        slides[currentSlide].classList.add('active');
+        dots[currentSlide].classList.add('active');
+    }
+
+    function startSlideshow() {
+        slideInterval = setInterval(nextSlide, 3000);
+    }
+
+    function resetInterval() {
+        clearInterval(slideInterval);
+        startSlideshow();
+    }
+
+    if (document.querySelector('.hero')) {
+        initSlideshow();
+    }
+});
